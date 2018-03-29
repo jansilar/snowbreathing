56c9cd6649d08fe24e4ec2011bda94210620c371
Filip <jezekf@gmail.com>
2018-03-29 23:32:31 +0200

    repairFlowData.m finished.

--- Scripts/getflows.m
+++ Scripts/getflows.m
@@ -23,62 +23,7 @@ pkg load signal
 %% filter the positive saturationf rom the flow
 % there are sometimes -0.1 and -0.2 invalid data reads
 
-
-% TODO filter out oulier peaks - three data points in a row, one is out of fucks
-
-
-
-%% prefilter for invalid data reads and its neighbours
-
-vargin = {flow, badReading = [-0.1, -0.2, -0.2], diffBounds = [-110, 40], maxWidth = 20};
-
-
-N = length(flow);
-a = flow != badReading';
-a = a(1, :) & a(1, :) & a(1, :);
-flowvalid = [false, a(1:end-2) & a(2: end-1) & a(3: end), false];
-
-flowv = interp1((1:N)(flowvalid), flow(flowvalid), 1:N, 'pchip');
-flowv(flowvalid) = flow(flowvalid);
-% find peaks of double derivative
-flowvdif = [flowv(1:end-1) - flowv(2:end), 0];
-%flowvdif2 = [0, flowvdif(1:end-1) - flowvdif(2:end)];
-%sat_loc = flowvdif2 > 50 | flowvdif2 < -50;
-
-%% filter out positive saturation - find a huge negative difference and search for a positive diff within some interval.
-sat = flowvalid;
-s1 = flowvdif < -110;
-s2 = [s1(1:end-1) & !(s1(2:end)), true];
-for i = 1:N-21;
-  if s2(i)
-    for j = i:i+20
-      if flowvdif(j) > 40
-        % found it!
-        sat(i:j+3) = false;
-        %break;
-      endif;
-    endfor;
-  endif;
-endfor;
-
-% TODO test spline inte
-flowv2 = interp1((1:N)(sat), flow(sat), 1:N, 'pchip');
-flowv2(sat) = flow(sat);
-
-%{
-figure(1)
-clf;hold on; plot(flow); 
-plot(flowv2, 'k'); 
-plot((1:N)(!sat), flowv(!sat), '*');
-plot((1:N)(sat_loc),flowvdif2(sat_loc), '*');
-%}
-%flowv2(sat) = flow(sat);
-clf;hold on; 
-plot(flow, '-g') ;
-plot(flowv2, 'b', 'LineWidth', 2); plot((1:N)((!sat)), flowv(!sat), 'r*');
-
-flow = flowv2;
-%plot(flowvdif);
+flow = repairFlowData(flow);
 
 
 
