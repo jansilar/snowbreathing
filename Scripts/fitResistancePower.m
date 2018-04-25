6e85cd563bf57070327d1bcf2d798f55e5888285
Filip Ježek <filip.jezek@lf1.cuni.cz>
2018-04-25 12:11:51 +0200

    Resistance fit identification for all data. Fixing the matrix sizes.

--- Scripts/fitResistancePower.m
+++ Scripts/fitResistancePower.m
@@ -4,18 +4,18 @@ function [powerCoeff, rms] = fitResistancePower(flow, pressure, doPlot)
 % X = 1:N;
 
 %% prepare positive quadrants and filter out half-negatives
-PQ = [pressure; flow];
+PQ = [pressure, flow];
 poslines = pressure > 0 & flow > 0;
 neglines = pressure < 0 & flow < 0;
-PQp = PQ(:, poslines );
-PQn = PQ(:, neglines );
+PQp = PQ(poslines,:);
+PQn = PQ(neglines,:);
 
 %% prepare the fit vectors
-PQ1Sided = [PQp, -PQn];
-pfit = PQ1Sided(1, :);
-ffit = PQ1Sided(2, :);
+PQ1Sided = [PQp; -PQn];
+pfit = PQ1Sided(:, 1);
+ffit = PQ1Sided(:, 2);
 
-[powerfit, validity] = fit(pfit', ffit', 'a*x^b');
+[powerfit, validity] = fit(pfit, ffit, 'a*x^b');
 powerCoeff = [powerfit.a, powerfit.b];
 rms = validity.rsquare;
 
@@ -24,8 +24,8 @@ if doPlot
     clf; hold on;
     xp = (0:0.1:max(pressure));
 
-    plot(PQp(1, :), PQp(2, :), 'bo');
-    plot(-PQn(1, :), -PQn(2, :), 'm*');
+    plot(PQp(:, 1), PQp(:, 2), 'bo');
+    plot(-PQn(:, 1), -PQn(:, 2), 'm*');
     % observe fit
     plot(xp, powerfit.a*xp.^powerfit.b, 'k-', 'linewidth', 4);
     legend('exhale', 'inhale', 'power fit')
